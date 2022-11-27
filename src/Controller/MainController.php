cac22f45c64fb616f4182d38679b8fb63f7d1482
bluegoku007 <selmenselmi5@gmail.com>
2022-11-27 12:25:50 +0100

    This is a new commit for what I originally planned to be amended

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="app_main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/login", name="app_main")
+     */
+    public function login(): Response
+    {
+        return $this->render('main/login.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
