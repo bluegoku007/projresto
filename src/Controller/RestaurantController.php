12f2064e3fc2f9a4d118912e963ea4844d5231dd
bluegoku007 <selmenselmi5@gmail.com>
2022-11-30 11:48:46 +0100

    j

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -2,19 +2,89 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
+use App\Form\RestaurantType;
+use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/restaurant")
+ */
 class RestaurantController extends AbstractController
 {
     /**
-     * @Route("/restaurant", name="app_restaurant")
+     * @Route("/", name="app_restaurant_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(RestaurantRepository $restaurantRepository): Response
     {
         return $this->render('restaurant/index.html.twig', [
-            'controller_name' => 'RestaurantController',
+            'restaurants' => $restaurantRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="app_restaurant_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, RestaurantRepository $restaurantRepository): Response
+    {
+        $restaurant = new Restaurant();
+        $form = $this->createForm(RestaurantType::class, $restaurant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $restaurantRepository->add($restaurant, true);
+
+            return $this->redirectToRoute('app_restaurant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('restaurant/new.html.twig', [
+            'restaurant' => $restaurant,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_restaurant_show", methods={"GET"})
+     */
+    public function show(Restaurant $restaurant): Response
+    {
+        return $this->render('restaurant/show.html.twig', [
+            'restaurant' => $restaurant,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_restaurant_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Restaurant $restaurant, RestaurantRepository $restaurantRepository): Response
+    {
+        $form = $this->createForm(RestaurantType::class, $restaurant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $restaurantRepository->add($restaurant, true);
+
+            return $this->redirectToRoute('app_restaurant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('restaurant/edit.html.twig', [
+            'restaurant' => $restaurant,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_restaurant_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Restaurant $restaurant, RestaurantRepository $restaurantRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$restaurant->getId(), $request->request->get('_token'))) {
+            $restaurantRepository->remove($restaurant, true);
+        }
+
+        return $this->redirectToRoute('app_restaurant_index', [], Response::HTTP_SEE_OTHER);
     }
 }
